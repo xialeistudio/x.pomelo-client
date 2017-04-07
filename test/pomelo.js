09fc2ddd236f4bb0d02a6e239df4efec1852301f
xialeistudio <1065890063@qq.com>
2017-04-07 11:24:17 +0800

    Update socket.io-client to 1.7.3

--- test/pomelo.js
+++ test/pomelo.js
@@ -12,18 +12,18 @@ describe('pomelo', function() {
   it('connect', function(done) {
     pomelo.init({
       host: '127.0.0.1',
-      port: 3014
+      port: 3010
     }, function() {
       done();
     });
   });
   it('request', function(done) {
-    pomelo.request('gate.gateHandler.queryEntry', 'hello pomelo', function(data) {
-      should(data.code).be.exactly(0);
+    pomelo.request("connector.entryHandler.entry", "hello pomelo", function(data) {
+      should(data.code).be.exactly(200);
       done();
     });
   });
   it('notify', function() {
-    pomelo.notify('gate.gateHandler.queryEntry', 'hello pomelo');
+    pomelo.notify('connector.entryHandler.entry', 'hello pomelo');
   });
 });
